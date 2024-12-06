cca425bd5b5d6cb03464f39d6a235f802515c8be
ILW8 <daohengliu@gmail.com>
2024-12-06 23:02:10 +0000

    download parts to prefix, skip already downloaded parts

--- download.py
+++ download.py
@@ -3,7 +3,7 @@ import math
 import requests
 import os
 import concurrent.futures
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunparse, urljoin
 
 from yt_dlp import YoutubeDL
 
@@ -22,19 +22,34 @@ def get_manifest_from_url(url: str) -> str | None:
 
 
 def download_file(uri: str, filename: str) -> None:
+    if os.path.exists(filename):
+        print(f"File {filename} already exists, skipping download.")
+        return
     resp = requests.get(uri)
     resp.raise_for_status()
     with open(filename, "wb") as out_fd:
         out_fd.write(resp.content)
 
 
-def download_from_playlist(uri: str, download_path_prefix="downloads", max_concurrent: int = 4) -> None:
+def download_from_playlist(uri: str, download_path_prefix="downloads", max_concurrent: int = 4) -> [str]:
+    """
+    Downloads all fragments from a .m3u8 playlist
+    :param uri:
+    :param download_path_prefix:
+    :param max_concurrent:
+    :return: List of paths to downloaded fragments
+    """
     assert uri.endswith(".m3u8")
-    if not os.path.exists(download_path_prefix.rstrip("/")) and not os.path.isdir(download_path_prefix.rstrip("/")):
-        os.makedirs(download_path_prefix.rstrip("/"))
+
+    safe_uri_path = "".join([c if c.isalnum() else "_" for c in "_".join(uri.split("/")[:-1])])
+    intermediate_path = os.path.join(download_path_prefix, safe_uri_path)
+
+    if not os.path.exists(intermediate_path):
+        os.makedirs(intermediate_path)
 
     print("Downloading manifest file...")
-    manifest_path = os.path.join(download_path_prefix, "playlist.m3u8")
+    manifest_path = os.path.join(intermediate_path, "playlist.m3u8")
+
     download_file(uri, manifest_path)
 
     with open(manifest_path, "r") as infile:
@@ -57,18 +72,27 @@ def download_from_playlist(uri: str, download_path_prefix="downloads", max_concu
 
     pad_amount = int(math.log10(len(fragments))) + 1
     count_completed = 0
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent) as executor:
-        safe_uri = "".join([c if c.isalnum() else "_" for c in uri])
-        intermediate_path = os.path.join(download_path_prefix, safe_uri)
-        if not os.path.exists(intermediate_path):
-            os.makedirs(intermediate_path)
 
-        futures_to_path = {executor.submit(download_file,
-                                           base_uri + fragment_url,
-                                           os.path.join(intermediate_path, fragment_url.split("/")[-1])): fragment_url
-                           for fragment_url in fragments}
+    fragment_paths: [str] = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent) as executor:
+        futures_to_path = {
+            executor.submit(
+                download_file,
+                '/'.join(s.strip('/') for s in (base_uri, fragment_url)),
+                (local_path := os.path.join(intermediate_path, fragment_url.split("/")[-1]))):
+                    (local_path, fragment_url)
+            for fragment_url in fragments
+        }
 
         for future in concurrent.futures.as_completed(futures_to_path):
             count_completed += 1
-            print(f"[{count_completed:>{pad_amount}}/{len(fragments)}] {futures_to_path[future]} completed")
+            if future.exception() is not None:
+                print(f"[{count_completed:>{pad_amount}}/{len(fragments)}] Error downloading "
+                      f"{futures_to_path[future][1]}: {future.exception()}")
+                continue
+            print(f"[{count_completed:>{pad_amount}}/{len(fragments)}] {futures_to_path[future][1]} completed")
+            fragment_paths.append(futures_to_path[future][0])
+
     print("Downloading fragments complete.")
+    return fragment_paths
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main():
         print("ffmpeg not found in PATH")
         return
     manifest_url = get_manifest_from_url("https://www.twitch.tv/videos/2315679372")
-    download_from_playlist(manifest_url)
+    fragments = download_from_playlist(manifest_url)
+    print(fragments)
 
 
 if __name__ == '__main__':
